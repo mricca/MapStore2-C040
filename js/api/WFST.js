7faced472aa1adf1274ea120543055dbffbd4548
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2017-06-07 12:29:38 +0200

    moved temp WFS-T api for this task

---
+++ js/api/WFST.js
@@ -0,0 +1,61 @@
+/**
+ * Copyright 2016, GeoSolutions Sas.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+const axios = require('../../MapStore2/web/client/libs/ajax');
+
+const urlUtil = require('url');
+const assign = require('object-assign');
+
+const {insert, deleteFeaturesByFilter, transaction} = require('../../MapStore2/web/client/utils/ogc/WFST');
+const {featureTypeSchema, getTypeName} = require('../../MapStore2/web/client/utils/ogc/WFS/base');
+
+const toData = (response) => {
+    if (typeof response.data !== 'object') {
+        return JSON.parse(response.data);
+    }
+    return response.data;
+};
+const Api = {
+    transaction: (baseUrl, operations, describeFeatureType, options) => {
+        const parsed = urlUtil.parse(baseUrl, true);
+        const url = urlUtil.format(assign({}, parsed, {
+            query: assign({
+                service: "WFS",
+                version: "1.1.0"
+            }, parsed.query)
+        }));
+        const body = transaction(operations, featureTypeSchema(describeFeatureType));
+        return axios.post(url, body, assign({headers: { 'Content-Type': 'application/xml'}}, options));
+    },
+    insert: (baseUrl, features, describeFeatureType, options) =>
+        Api.transaction(baseUrl, [insert(features, describeFeatureType)], describeFeatureType, options),
+    /**
+     * Simpler version of describeFeatureType, only json format supported
+     * @param  {string} baseUrl         Base ows services url
+     * @param  {string} featureTypeName the name of the feature type
+     * @param  {Object} [params={}]     Other parameters
+     * @param  {object} [options]         Options for the request
+     * @return {Promise}                the request promise
+     */
+    describeFeatureType: (baseUrl, featureTypeName, params={}, options) => {
+        const parsed = urlUtil.parse(baseUrl, true);
+        const url = urlUtil.format(assign({}, parsed, {
+            query: assign({
+                service: "WFS",
+                version: "1.1.0",
+                request: "DescribeFeatureType",
+                typeName: featureTypeName,
+                outputFormat: "application/json"
+            }, parsed.query, params)
+        }));
+        return axios.get(url, options).then(toData);
+    },
+    deleteByFilter: (baseUrl, filter, describeFeatureType, options) =>
+        Api.transaction(baseUrl, [deleteFeaturesByFilter(filter, getTypeName(describeFeatureType))], describeFeatureType, options)
+};
+
+module.exports = Api;
