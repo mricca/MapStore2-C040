b0fa91c72d6faa1750ce5bd312607c63137402e2
Riccardo Mari <rmari76@gmail.com>
2017-11-03 15:30:06 +0100

    Update style-loader webpack

--- karma.conf.single-run.js
+++ karma.conf.single-run.js
@@ -2,22 +2,25 @@ var path = require("path");
 module.exports = function karmaConfig(config) {
     config.set({
 
-        browsers: [ 'Chrome' ],
+        browsers: ['Chrome'],
 
         singleRun: true,
 
-        frameworks: [ 'mocha' ],
+        frameworks: ['mocha'],
 
         files: [
             'tests.webpack.js',
-            { pattern: './js/test-resources/**/*', included: false }
+            {
+                pattern: './js/test-resources/**/*',
+                included: false
+            }
         ],
 
         preprocessors: {
-            'tests.webpack.js': [ 'webpack', 'sourcemap' ]
+            'tests.webpack.js': ['webpack', 'sourcemap']
         },
 
-        reporters: [ 'mocha', 'coverage', 'coveralls' ],
+        reporters: ['mocha', 'coverage', 'coveralls'],
 
         junitReporter: {
             outputDir: './js/target/karma-tests-results',
@@ -26,36 +29,74 @@ module.exports = function karmaConfig(config) {
 
         coverageReporter: {
             dir: './coverage/',
-            reporters: [
-                { type: 'html', subdir: 'report-html' },
-                { type: 'cobertura', subdir: '.', file: 'cobertura.txt' },
-                { type: 'lcovonly', subdir: '.' }
+            reporters: [{
+                    type: 'html',
+                    subdir: 'report-html'
+                },
+                {
+                    type: 'cobertura',
+                    subdir: '.',
+                    file: 'cobertura.txt'
+                },
+                {
+                    type: 'lcovonly',
+                    subdir: '.'
+                }
             ],
             instrumenterOptions: {
-                istanbul: { noCompact: true }
+                istanbul: {
+                    noCompact: true
+                }
             }
         },
 
         webpack: {
             devtool: 'eval',
             module: {
-                rules: [
-                    {
+                rules: [{
                         test: /\.jsx?$/,
                         exclude: /(__tests__|node_modules|legacy)\/|(__tests__|node_modules|legacy)\\|webpack\.js|utils\/(openlayers|leaflet)/,
                         enforce: "pre",
-                        use: [
-                            {
-                                loader: 'babel-istanbul-loader'
-                            }
-                        ]
+                        use: [{
+                            loader: 'babel-istanbul-loader'
+                        }]
                     },
-                    { test: /\.jsx?$/, exclude: /(ol\.js$|node_modules)/, loader: 'babel-loader', include: [path.join(__dirname, "js"), path.join(__dirname, "MapStore2", "web", "client")] },
-                    { test: /\.css$/, loader: 'style!css-loader'},
-                    { test: /\.less$/, loader: "style!css!less-loader" },
-                    { test: /\.woff(2)?(\?v=[0-9].[0-9].[0-9])?$/, loader: "url-loader?mimetype=application/font-woff" },
-                    { test: /\.(ttf|eot|svg)(\?v=[0-9].[0-9].[0-9])?$/, loader: "file-loader?name=[name].[ext]" },
-                    { test: /\.(png|jpg|gif|svg)$/, loader: 'url-loader?name=[path][name].[ext]&limit=8192'} // inline base64 URLs for <=8k images, direct URLs for the rest
+                    {
+                        test: /\.jsx?$/,
+                        exclude: /(ol\.js$|node_modules)/,
+                        loader: 'babel-loader',
+                        include: [path.join(__dirname, "js"), path.join(__dirname, "MapStore2", "web", "client")]
+                    },
+                    {
+                        test: /\.css$/,
+                        use: [{
+                            loader: 'style-loader'
+                        }, {
+                            loader: 'css-loader'
+                        }]
+                    },
+                    {
+                        test: /\.less$/,
+                        use: [{
+                            loader: 'style-loader'
+                        }, {
+                            loader: 'css-loader'
+                        }, {
+                            loader: 'less-loader'
+                        }]
+                    },
+                    {
+                        test: /\.woff(2)?(\?v=[0-9].[0-9].[0-9])?$/,
+                        loader: "url-loader?mimetype=application/font-woff"
+                    },
+                    {
+                        test: /\.(ttf|eot|svg)(\?v=[0-9].[0-9].[0-9])?$/,
+                        loader: "file-loader?name=[name].[ext]"
+                    },
+                    {
+                        test: /\.(png|jpg|gif|svg)$/,
+                        loader: 'url-loader?name=[path][name].[ext]&limit=8192'
+                    } // inline base64 URLs for <=8k images, direct URLs for the rest
                 ]
             },
             resolve: {
